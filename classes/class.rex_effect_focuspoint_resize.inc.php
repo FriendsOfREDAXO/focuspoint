1fdceb41a9b1810fc5d5e6cb8998c233a84066a7
Jan Kristinus <jan.kristinus@yakamara.de>
2014-10-01 01:32:12 +0200

    focus point effect ergänzt, in den install process eigebaut und config entsprechend angepasst

--- classes/class.rex_effect_focuspoint_resize.inc.php
+++ classes/class.rex_effect_focuspoint_resize.inc.php
@@ -1,20 +1,16 @@
 <?php
 
-// Todo:
-// - Vergrš§ern erlauben oder nicht. aber eher als Modul einsetzen, also
-// fit
-
 class rex_effect_focuspoint_resize extends rex_effect_abstract
 {
-    var $options;
-    var $script;
+  var $options;
+  var $script;
 
-    function rex_effect_focuspoint_resize()
-    {
+  function rex_effect_focuspoint_resize()
+  {
 
-        $this->options = array('maximum', 'minimum', 'exact');
+    $this->options = array('maximum', 'minimum', 'exact');
 
-        $this->script = '
+    $this->script = '
 <script type="text/javascript">
 <!--
 
@@ -37,146 +33,199 @@ class rex_effect_focuspoint_resize extends rex_effect_abstract
 
 //--></script>';
 
+  }
+
+  function execute()
+  {
+
+    if (!$this->image->isImage()) {
+      return false;
     }
 
-    function execute()
-    {
+    $gdimage = & $this->image->getImage();
+    $w = $this->image->getWidth();
+    $h = $this->image->getHeight();
 
-        if (!$this->image->isImage()) {
-            return false;
+    $filename = $this->image->getFileName();
+    if ( ($im_image = OOMedia::getMediaByName($filename)) ) {
+
+      $focuspoint_data = explode(",", $im_image->getValue('med_focuspoint_data'), 2);
+      if (count($focuspoint_data) == 2) {
+
+        // Mittelpunkt finden
+        $x = ceil($w/2);
+        $y = ceil($h/2);
+
+        // focusoffsets einarbeiten
+        $fp_w = $focuspoint_data[0];
+        $fp_h = $focuspoint_data[1];
+
+        // Neuen Mittelpunkt finden
+        $nx = $x + ceil($x * $fp_w);
+        $ny = $y - ceil($y * $fp_h);
+
+        // Abstand zum Rand herausfinden
+        $nw = $w - $nx; // 1/2 Breite
+        if ($fp_w < 0) {
+          $nw = $nx; // 1/2 Breite
         }
 
-        $gdimage = & $this->image->getImage();
-        $w = $this->image->getWidth();
-        $h = $this->image->getHeight();
-
-        if (!isset($this->params['style']) || !in_array($this->params['style'], $this->options)) {
-            $this->params['style'] = 'maximum';
+        $nh = $ny; // 1/2 Breite
+        if ($fp_h < 0) {
+          $nh = $h - $ny; // 1/2 Breite
         }
 
-        // relatives resizen
-        if (substr(trim($this->params['width']), -1) === '%') {
-            $this->params['width'] = round($w * (rtrim($this->params['width'], '%') / 100));
-        }
-        if (substr(trim($this->params['height']), -1) === '%') {
-            $this->params['height'] = round($h * (rtrim($this->params['height'], '%') / 100));
-        }
-
-        if ($this->params['style'] == 'maximum') {
-            $this->resizeMax($w, $h);
-        } elseif ($this->params['style'] == 'minimum') {
-            $this->resizeMin($w, $h);
-        } else {
-            // warp => nichts tun
-        }
-
-        // ----- not enlarge image
-        if ($w <= $this->params['width'] && $h <= $this->params['height'] && $this->params['allow_enlarge'] == 'not_enlarge') {
-            $this->params['width'] = $w;
-            $this->params['height'] = $h;
-            $this->keepTransparent($gdimage);
-            return;
-        }
-
-        if (!isset($this->params['width'])) {
-            $this->params['width'] = $w;
-        }
-
-        if (!isset($this->params['height'])) {
-            $this->params['height'] = $h;
-        }
+        $npx = $nx - $nw;
+        $npy = $ny - $nh;
+        $nw = $nw * 2;
+        $nh = $nh * 2;
 
         if (function_exists('ImageCreateTrueColor')) {
-            $des = @ImageCreateTrueColor($this->params['width'], $this->params['height']);
+          $des = @ImageCreateTrueColor($nw, $nh);
         } else {
-            $des = @ImageCreate($this->params['width'], $this->params['height']);
-        }
-
-        if (!$des) {
-            return;
+          $des = @ImageCreate($nw, $nh);
         }
 
         $this->keepTransparent($des);
-        imagecopyresampled($des, $gdimage, 0, 0, 0, 0, $this->params['width'], $this->params['height'], $w, $h);
+        imagecopyresampled($des, $gdimage, 0, 0, $npx, $npy, $nw, $nh, $nw, $nh);
 
         $gdimage = $des;
         $this->image->refreshDimensions();
+
+        $w = $nw;
+        $h = $nh;
+
+      }
+
     }
 
-    function resizeMax($w, $h)
-    {
-        if (!empty($this->params['height']) && !empty($this->params['width'])) {
-            $img_ratio  = $w / $h;
-            $resize_ratio = $this->params['width'] / $this->params['height'];
-
-            if ($img_ratio >= $resize_ratio) {
-                // --- width
-                $this->params['height'] = ceil($this->params['width'] / $w * $h);
-            } else {
-                // --- height
-                $this->params['width']  = ceil($this->params['height'] / $h * $w);
-            }
-        } elseif (!empty($this->params['height'])) {
-            $img_factor  = $h / $this->params['height'];
-            $this->params['width'] = ceil($w / $img_factor);
-        } elseif (!empty($this->params['width'])) {
-            $img_factor  = $w / $this->params['width'];
-            $this->params['height'] = ceil($h / $img_factor);
-        }
+    if (!isset($this->params['style']) || !in_array($this->params['style'], $this->options)) {
+      $this->params['style'] = 'maximum';
     }
 
-    function resizeMin($w, $h)
-    {
-        if (!empty($this->params['height']) && !empty($this->params['width'])) {
-            $img_ratio  = $w / $h;
-            $resize_ratio = $this->params['width'] / $this->params['height'];
-
-            if ($img_ratio < $resize_ratio) {
-                // --- width
-                $this->params['height'] = ceil($this->params['width'] / $w * $h);
-            } else {
-                // --- height
-                $this->params['width']  = ceil($this->params['height'] / $h * $w);
-            }
-        } elseif (!empty($this->params['height'])) {
-            $img_factor  = $h / $this->params['height'];
-            $this->params['width'] = ceil($w / $img_factor);
-        } elseif (!empty($this->params['width'])) {
-            $img_factor  = $w / $this->params['width'];
-            $this->params['height'] = ceil($h / $img_factor);
-        }
+    // relatives resizen
+    if (substr(trim($this->params['width']), -1) === '%') {
+      $this->params['width'] = round($w * (rtrim($this->params['width'], '%') / 100));
+    }
+    if (substr(trim($this->params['height']), -1) === '%') {
+      $this->params['height'] = round($h * (rtrim($this->params['height'], '%') / 100));
     }
 
-    function getParams()
-    {
-        global $REX, $I18N;
-
-        return array(
-            array(
-                'label' => $I18N->msg('imanager_effect_resize_width'),
-                'name' => 'width',
-                'type' => 'int',
-            ),
-            array(
-                'label' => $I18N->msg('imanager_effect_resize_height'),
-                'name' => 'height',
-                'type' => 'int'
-            ),
-            array(
-                'label' => $I18N->msg('imanager_effect_resize_style'),
-                'name' => 'style',
-                'type'  => 'select',
-                'options' => $this->options,
-                'default' => 'fit',
-                'suffix' => $this->script
-            ),
-            array(
-                'label' => $I18N->msg('imanager_effect_resize_imgtosmall'),
-                'name' => 'allow_enlarge',
-                'type' => 'select',
-                'options' => array('enlarge', 'not_enlarge'),
-                'default' => 'enlarge',
-            ),
-        );
+    if ($this->params['style'] == 'maximum') {
+      $this->resizeMax($w, $h);
+    } elseif ($this->params['style'] == 'minimum') {
+      $this->resizeMin($w, $h);
+    } else {
+      // warp => nichts tun
     }
+
+    // ----- not enlarge image
+    if ($w <= $this->params['width'] && $h <= $this->params['height'] && $this->params['allow_enlarge'] == 'not_enlarge') {
+      $this->params['width'] = $w;
+      $this->params['height'] = $h;
+      $this->keepTransparent($gdimage);
+      return;
+    }
+
+    if (!isset($this->params['width'])) {
+      $this->params['width'] = $w;
+    }
+
+    if (!isset($this->params['height'])) {
+      $this->params['height'] = $h;
+    }
+
+    if (function_exists('ImageCreateTrueColor')) {
+      $des = @ImageCreateTrueColor($this->params['width'], $this->params['height']);
+    } else {
+      $des = @ImageCreate($this->params['width'], $this->params['height']);
+    }
+
+    if (!$des) {
+      return;
+    }
+
+    $this->keepTransparent($des);
+    imagecopyresampled($des, $gdimage, 0, 0, 0, 0, $this->params['width'], $this->params['height'], $w, $h);
+
+    $gdimage = $des;
+    $this->image->refreshDimensions();
+  }
+
+  function resizeMax($w, $h)
+  {
+    if (!empty($this->params['height']) && !empty($this->params['width'])) {
+      $img_ratio  = $w / $h;
+      $resize_ratio = $this->params['width'] / $this->params['height'];
+
+      if ($img_ratio >= $resize_ratio) {
+        // --- width
+        $this->params['height'] = ceil($this->params['width'] / $w * $h);
+      } else {
+        // --- height
+        $this->params['width']  = ceil($this->params['height'] / $h * $w);
+      }
+    } elseif (!empty($this->params['height'])) {
+      $img_factor  = $h / $this->params['height'];
+      $this->params['width'] = ceil($w / $img_factor);
+    } elseif (!empty($this->params['width'])) {
+      $img_factor  = $w / $this->params['width'];
+      $this->params['height'] = ceil($h / $img_factor);
+    }
+  }
+
+  function resizeMin($w, $h)
+  {
+    if (!empty($this->params['height']) && !empty($this->params['width'])) {
+      $img_ratio  = $w / $h;
+      $resize_ratio = $this->params['width'] / $this->params['height'];
+
+      if ($img_ratio < $resize_ratio) {
+        // --- width
+        $this->params['height'] = ceil($this->params['width'] / $w * $h);
+      } else {
+        // --- height
+        $this->params['width']  = ceil($this->params['height'] / $h * $w);
+      }
+    } elseif (!empty($this->params['height'])) {
+      $img_factor  = $h / $this->params['height'];
+      $this->params['width'] = ceil($w / $img_factor);
+    } elseif (!empty($this->params['width'])) {
+      $img_factor  = $w / $this->params['width'];
+      $this->params['height'] = ceil($h / $img_factor);
+    }
+  }
+
+  function getParams()
+  {
+    global $REX, $I18N;
+
+    return array(
+    array(
+    'label' => $I18N->msg('imanager_effect_resize_width'),
+    'name' => 'width',
+    'type' => 'int',
+    ),
+    array(
+    'label' => $I18N->msg('imanager_effect_resize_height'),
+    'name' => 'height',
+    'type' => 'int'
+    ),
+    array(
+    'label' => $I18N->msg('imanager_effect_resize_style'),
+    'name' => 'style',
+    'type'  => 'select',
+    'options' => $this->options,
+    'default' => 'fit',
+    'suffix' => $this->script
+    ),
+    array(
+    'label' => $I18N->msg('imanager_effect_resize_imgtosmall'),
+    'name' => 'allow_enlarge',
+    'type' => 'select',
+    'options' => array('enlarge', 'not_enlarge'),
+    'default' => 'enlarge',
+    ),
+    );
+  }
 }
--- config.inc.php
+++ config.inc.php
@@ -1,38 +1,31 @@
 <?php
 
-
 $mypage = 'focuspoint';
 
-if($REX['REDAXO'])
-{
+if($REX['REDAXO']) {
 
-  $I18N->appendFile(dirname(__FILE__) . '/lang/');
-  $REX['ADDON']['version'][$mypage] = '0.5';
-  $REX['ADDON']['author'][$mypage] = 'Oliver Kreischer, Daniel Weitenauer';
-  $REX['ADDON']['name'][$mypage] = $I18N->msg('fp_title');
+    $I18N->appendFile(dirname(__FILE__) . '/lang/');
+    $REX['ADDON']['version'][$mypage] = '0.5';
+    $REX['ADDON']['author'][$mypage] = 'Oliver Kreischer, Daniel Weitenauer';
+    $REX['ADDON']['name'][$mypage] = $I18N->msg('fp_title');
 
-  $I18N->appendFile(dirname(__FILE__) . '/lang/');
+    $I18N->appendFile(dirname(__FILE__) . '/lang/');
 
-  include $REX["INCLUDE_PATH"]."/addons/focuspoint/classes/class.rex_focuspoint.inc.php";
+    include $REX["INCLUDE_PATH"]."/addons/focuspoint/classes/class.rex_focuspoint.inc.php";
 
-  rex_register_extension('MEDIA_ADDED', 'rex_focuspoint::set_media');
-  rex_register_extension('MEDIA_UPDATED', 'rex_focuspoint::set_media');
-  rex_register_extension('MEDIA_FORM_EDIT', 'rex_focuspoint::show_form_info');
+    rex_register_extension('MEDIA_ADDED', 'rex_focuspoint::set_media');
+    rex_register_extension('MEDIA_UPDATED', 'rex_focuspoint::set_media');
+    rex_register_extension('MEDIA_FORM_EDIT', 'rex_focuspoint::show_form_info');
 
-  $rootPage = new rex_be_page($I18N->msg('fp_setup'), array(
-      'page' => $mypage,
-      'subpage' => ''
-    )
-  );
-  $rootPage->setHref('index.php?page=focuspoint');
+    $helpPage = new rex_be_page($I18N->msg('fp_help'), array(
+            'page' => $mypage,
+            'subpage' => ''
+        )
+    );
+    $helpPage->setHref('index.php?page=focuspoint');
 
-  // Subpages
-  $REX['ADDON']['pages'][$mypage] = array(
-    $rootPage
-  );
-
-
+    $REX['ADDON']['pages'][$mypage] = array(
+        $helpPage
+    );
 
 }
-
-?>
