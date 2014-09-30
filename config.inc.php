38aaa09a42ab62e30678acf00906cbdac4b7374c
Jan Kristinus <jan.kristinus@yakamara.de>
2014-09-30 17:47:45 +0200

    Sprache hinzugefügt, Setupseite ergänzt, Seiten eingebunden

--- config.inc.php
+++ config.inc.php
@@ -1,16 +1,37 @@
 <?php
 
+
 $mypage = 'focuspoint';
-$REX['ADDON']['version'][$mypage] = '0.5';
-$REX['ADDON']['author'][$mypage] = 'Oliver Kreischer, Daniel Weitenauer';
 
 if($REX['REDAXO'])
 {
+
+  $I18N->appendFile(dirname(__FILE__) . '/lang/');
+  $REX['ADDON']['version'][$mypage] = '0.5';
+  $REX['ADDON']['author'][$mypage] = 'Oliver Kreischer, Daniel Weitenauer';
+  $REX['ADDON']['name'][$mypage] = $I18N->msg('fp_title');
+
+  $I18N->appendFile(dirname(__FILE__) . '/lang/');
+
   include $REX["INCLUDE_PATH"]."/addons/focuspoint/classes/class.rex_focuspoint.inc.php";
 
   rex_register_extension('MEDIA_ADDED', 'rex_focuspoint::set_media');
   rex_register_extension('MEDIA_UPDATED', 'rex_focuspoint::set_media');
   rex_register_extension('MEDIA_FORM_EDIT', 'rex_focuspoint::show_form_info');
+
+  $rootPage = new rex_be_page($I18N->msg('fp_setup'), array(
+      'page' => $mypage,
+      'subpage' => ''
+    )
+  );
+  $rootPage->setHref('index.php?page=focuspoint');
+
+  // Subpages
+  $REX['ADDON']['pages'][$mypage] = array(
+    $rootPage
+  );
+
+
 
 }
 
---
+++ pages/setup.inc.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+echo '
+<div class="rex-addon-output">
+  <h2 class="rex-hl2">Beschreibung</h2>
+
+  <div class="rex-addon-content">
+
+// Modul installieren
+
+// IM Effekt installieren
+
+  </div>
+</div>';
+
+
+?>
+
+
+
