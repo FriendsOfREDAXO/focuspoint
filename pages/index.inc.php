82b535ea566328b780e3f4e3f2a0def040cc6bac
Jan Kristinus <jan.kristinus@yakamara.de>
2014-10-01 01:31:22 +0200

    hilfsseite erstellt.

--- pages/index.inc.php
+++ pages/index.inc.php
@@ -9,18 +9,17 @@ require $REX['INCLUDE_PATH'] . '/layout/top.php';
 $page    = rex_request('page', 'string');
 $subpage = rex_request('subpage', 'string');
 $func    = rex_request('func', 'string');
-$oid     = rex_request('oid', 'int');
 
 rex_title($I18N->msg('fp_title'), $REX['ADDON']['pages']['focuspoint']);
 
-echo "\n  <div class=\"rex-addon-output-v2\">\n  ";
+echo '<div class="rex-addon-output-v2">';
 
-if (!in_array($subpage, array('setup'))) {
-    $subpage = 'setup';
+if (!in_array($subpage, array('help'))) {
+    $subpage = 'help';
 }
 
 require $REX['INCLUDE_PATH'] . '/addons/focuspoint/pages/' . $subpage . '.inc.php';
 
-echo "\n  </div>";
+echo '</div>';
 
 require $REX['INCLUDE_PATH'] . '/layout/bottom.php';
