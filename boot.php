9831dbd2f765f4f5582512924ba7f4abfd416946
christophboecker <christoph_boecker@t-online.de>
2018-06-07 18:27:05 +0200

    Includes wider gelöscht

--- boot.php
+++ boot.php
@@ -7,14 +7,12 @@ if (rex::isBackend()) {
         rex_view::addCssFile($this->getAssetsUrl('focuspoint.css'));
 
         rex_extension::register('MEDIA_FORM_EDIT', function( $media ) {
-            include_once( 'functions/class.rex_focuspoint.inc.php');
             return rex_focuspoint::show_form_info( $media );
         });
 
 
         rex_extension::register('MEDIA_FORM_ADD', function( $media ) {
-            include_once( 'functions/class.rex_focuspoint.inc.php');
-            rex_focuspoint::remove_inputs( $media );
+             rex_focuspoint::remove_inputs( $media );
         });
     }
 
