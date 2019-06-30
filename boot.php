302b3ebbb40ea9f7fb1433d2833c021fc19fca4e
christophboecker <christoph_boecker@t-online.de>
2019-06-30 12:25:16 +0200

    V2.1: Codeblöcke ausgelagert für effizenteren Start
    Die Codeblöcke müssen nicht mehr bei jedem Start der REX-Instanz evaluiert werden. Sie sind nach `focuspoint_bot.php` ausgelagert

--- boot.php
+++ boot.php
@@ -3,7 +3,7 @@
  *  This file is part of the REDAXO-AddOn "focuspoint".
  *
  *  @author      FriendsOfREDAXO @ GitHub <https://github.com/FriendsOfREDAXO/focuspoint>
- *  @version     2.0
+ *  @version     2.1
  *  @copyright   FriendsOfREDAXO <https://friendsofredaxo.github.io/>
  *
  *  For the full copyright and license information, please view the LICENSE
@@ -11,7 +11,8 @@
  *
  *  ------------------------------------------------------------------------------------------------
  *
- *
+ *  Teile der Verarbeitung sind - bis auf die Abfragen - ausgelagert, um die Code-Übersetzung
+ *  nur durchzuführen, wenn es notwendig ist, jedoch nicht bei jedem Aufruf.
  */
 
 if (rex::isBackend())
@@ -21,47 +22,33 @@ if (rex::isBackend())
     {
         case 'mediapool/media':
             // provide support for media detail-page
-            rex_view::addCssFile($this->getAssetsUrl('focuspoint.min.css'));
-            rex_view::addJsFile($this->getAssetsUrl('focuspoint.min.js'));
-            rex_extension::register('MEDIA_DETAIL_SIDEBAR', 'focuspoint::show_sidebar');
-            rex_extension::register('METAINFO_CUSTOM_FIELD', 'focuspoint::customfield' );
+            focuspoint_boot::mediaDetailPage( $this );
             break;
 
         case 'metainfo/articles':
         case 'metainfo/categories':
         case 'metainfo/clangs':
             // delete focuspoint-datatype from html-select for articles/categories/clangs
-            if( ($func = rex_request('func', 'string')) && $func != 'delete' )
-            {
-                rex_extension::register( 'METAINFO_TYPE_FIELDS', function( rex_extension_point $ep ){
-                    echo '<script>$(document).ready(function(){ $("select[name$=\'[type_id]\'] option:contains(\''.rex_effect_abstract_focuspoint::META_FIELD_TYPE.'\')").detach();});</script>';
-                });
-            }
+            focuspoint_boot::metainfoDefault();
             break;
 
         case 'metainfo/media':
+        dump($this);
             // prevent deletion of meta-fields still in use by effects
-            if( rex_request('func', 'string') == 'delete' )
-            {
-                rex_extension::register( 'PACKAGES_INCLUDED', function( rex_extension_point $ep ){
-                    if( $result = focuspoint::metafield_is_in_use( rex_request('field_id', 'int', 0) ) )
-                    {
-                        $_REQUEST['func'] = '';
-                        rex_extension::register('PAGE_TITLE_SHOWN', function(rex_extension_point $ep) use ($result) {
-                            $ep->setSubject(rex_view::error($result) . $ep->getSubject());
-                        });
-                    }
-                });
-            }
+            // limit changing the default-focuspoint-metafield: fieldname, fieldtype, no delete
+            // don´t remove the default-Metafield
+            focuspoint_boot::metainfoMedia();
+            break;
+
+        case 'media_manager/types':
+            // prevent deletion and editing of mediamanager-type used by focuspoint
+            focuspoint_boot::media_managerTypes();
+            break;
+
         case 'packages':
             // prevent deactivation if in use by effects
             // effective only in dialog-mode via AddOn-administration-page
-            if( rex_request('package', 'string') == $this->getName()
-                && isset($_REQUEST['rex-api-call'])
-                && $_REQUEST['rex-api-call'] == 'package' )
-            {
-                $_REQUEST['rex-api-call'] = 'focuspoint_package';
-            }
+            focuspoint_boot::packages( $this );
             break;
     }
 
@@ -69,4 +56,4 @@ if (rex::isBackend())
 
 rex_media_manager::addEffect('rex_effect_focuspoint_fit');
 // deprecated:
-rex_media_manager::addEffect('rex_effect_focuspoint_resize');
+// rex_media_manager::addEffect('rex_effect_focuspoint_resize');
